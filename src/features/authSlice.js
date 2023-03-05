9154a33a1487f373e5b20b1399588fe9d9326167
shegz101 <66688833+shegz101@users.noreply.github.com>
2023-03-05 16:17:50 +0100

    mode-of-retrieving-username-updated

--- src/features/authSlice.js
+++ src/features/authSlice.js
@@ -4,6 +4,7 @@ import { createSlice } from '@reduxjs/toolkit';
 const initialState = {
   user: null,
   trailer_Id: "",
+  user_name: "",
 };
 
 export const authSlice = createSlice({
@@ -19,17 +20,23 @@ export const authSlice = createSlice({
     },
     trail: (state, action) => {
       state.trailer_Id = action.payload;
+    },
+    populateUser: (state, action) => {
+      state.user_name = action.payload;
     } 
   },
 });
 
-export const { login, logout, trail } = authSlice.actions;
+export const { login, logout, trail, populateUser } = authSlice.actions;
 
 // The function below is called a selector and allows us to select a value from
 // the state. Selectors can also be defined inline where they're used instead of
 // in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
 export const selectUser = (state) => state.user.user;
 export const selectId = (state) => state.user.trailer_Id;
+export const selectName = (state) => state.user.user_name;
+
+
 
 // We can also write thunks by hand, which may contain both sync and async logic.
 // Here's an example of conditionally dispatching actions based on current state.
