4ee56b14d64316d80aeb5c5af67ba7d1bcbef002
shegz101 <bobatesegun3@gmail.com>
2023-04-01 23:10:13 +0100

    linked-banner-to-search-page

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,56 +1,74 @@
-import React, {useState,useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 // import Typewriter from "typewriter-effect";
-// import { TbPlayerPlay } from 'react-icons/tb';
-import '../styles/Banner.css';
-import axios_fetch from '../api/axios_fetch';
-import request_data from '../api/request_data';
+// import { TbPlayerPlay } from "react-icons/tb";
+import "../styles/Banner.css";
+import axios_fetch from "../api/axios_fetch";
+import request_data from "../api/request_data";
 
 const Banner = () => {
-    const [bannermovie, setBannerMovie] = useState([]);
-    
-    const fetch_movie = async () => {
-        const resp = await axios_fetch.get(request_data.Trending);
-        setBannerMovie(resp.data.results[Math.floor(Math.random() * resp.data.results.length - 1)]);
+  const [bannermovie, setBannerMovie] = useState([]);
+
+  const fetch_movie = async () => {
+    const resp = await axios_fetch.get(request_data.Trending);
+    setBannerMovie(
+      resp.data.results[
+        Math.floor(Math.random() * resp.data.results.length - 1)
+      ]
+    );
+  };
+
+  //function to reduce the length of movie description
+  const truncate = (string) => {
+    if (string?.length > 100) {
+      return string.slice(0, 100) + " ...";
     }
+  };
 
-    //function to reduce the length of movie description
-    const truncate = (string) => {
-        if(string?.length > 100) {
-            return string.slice(0, 100) + ' ...';
-        }
-    }
+  useEffect(() => {
+    fetch_movie();
+  }, []);
 
-    useEffect(() => {
-        fetch_movie();
-        return () => fetch_movie();
-    },[])
-    
-    return (
-        <header className="banner__image" style={{backgroundSize: 'cover', 
-            backgroundImage: `url('https://image.tmdb.org/t/p/original/${bannermovie?.backdrop_path || bannermovie?.poster_path}')`,
-            backgroundPosition: "center center",
-            }}>
-            
-            <div className="banner-contents">
-                {/* <Typewriter 
-                    onInit={(typewriter)=> {
-                    typewriter.typeString(`<h1 className='movie__name'>${bannermovie?.title || bannermovie?.name || bannermovie?.original_name}</h1>`).stop().start();
-                }}/> */}
-                <h1 className='movie__name'>{bannermovie?.title || bannermovie?.name || bannermovie?.original_name}</h1>
-                <div className="buttons">
-                    <button className='button__play'>Play</button>
-                    <button className='button__add'>Add to List</button>
-                </div>
-                {/* <Typewriter 
-                    onInit={(typewriter)=> {
-                    typewriter.typeString(`<p className='movie__description'>${truncate(bannermovie?.overview)}</p>`).stop().start();
-                }}/> */}
-                <p className='movie__description'>{truncate(bannermovie?.overview)}</p>
-            </div>
+  return (
+    <header
+      className="banner__image"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url('https://image.tmdb.org/t/p/original/${
+          bannermovie?.backdrop_path || bannermovie?.poster_path
+        }')`,
+        backgroundPosition: "center center",
+      }}
+    >
+      <div className="banner-contents">
+        {/* <Typewriter
+          onInit={(typewriter) => {
+            typewriter
+              .typeString(
+                `<h1 className='movie__name'>${
+                  bannermovie?.title ||
+                  bannermovie?.name ||
+                  bannermovie?.original_name
+                }</h1>`
+              )
+              .stop()
+              .start();
+          }}
+        /> */}
+        <h1 className="movie__name">
+          {bannermovie?.title ||
+            bannermovie?.name ||
+            bannermovie?.original_name}
+        </h1>
+        <div className="buttons">
+          <button className="button__play">Play</button>
+          <button className="button__add">Add to List</button>
+        </div>
+        <p className="movie__description">{truncate(bannermovie?.overview)}</p>
+      </div>
 
-            <div className="invisible__div"/>
-        </header>
-    )
-}
+      <div className="invisible__div" />
+    </header>
+  );
+};
 
 export default Banner;
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,65 +1,72 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // import ClipLoader from "react-spinners/ClipLoader";
-import axios_fetch from '../api/axios_fetch';
-import request_data from '../api/request_data';
-import {BiSearch} from 'react-icons/bi';
-import {MdOutlineArrowBackIosNew} from 'react-icons/md';
-import '../styles/search.css';
-import Error from './Error';
-import { useNavigate } from 'react-router-dom';
-import ResultCard from './ResultCard';
+import axios_fetch from "../api/axios_fetch";
+import request_data from "../api/request_data";
+import { BiSearch } from "react-icons/bi";
+import { MdOutlineArrowBackIosNew } from "react-icons/md";
+import "../styles/search.css";
+import Error from "./Error";
+import { useNavigate } from "react-router-dom";
+import ResultCard from "./ResultCard";
 
 const Search = () => {
-    const [searchinput, setSearchInput] = useState("");
-    const [searchresponse, setSearchResponse] = useState([]);
-    const url = request_data.search;
-    const navigate = useNavigate();
-    
-    const fetch_search = async () => {
-        const resp = await axios_fetch.get(`${url}&query=${searchinput}`);
-        setSearchResponse(resp.data.results);
-        setSearchInput("");
-    }
+  const [searchinput, setSearchInput] = useState("");
+  const [searchresponse, setSearchResponse] = useState([]);
+  const url = request_data.search;
+  const navigate = useNavigate();
 
-    const result__lenght = searchresponse.length;
+  const fetch_search = async () => {
+    const resp = await axios_fetch.get(`${url}&query=${searchinput}`);
+    setSearchResponse(resp.data.results);
+    setSearchInput("");
+  };
 
-    console.log(searchresponse);
-    return ( 
-        <div className='search__section'>
-            <div className="search__input__section">
-                <div className='navigate_icon' onClick={() => navigate(-1)}>
-                    <MdOutlineArrowBackIosNew style={{color:'white'}} className="nav__icon"/>
-                </div>
-                <div className="search__field">
-                    <div className='input__gloss'>
-                        <BiSearch className='search__icon' style={{color:'white'}}/>
-                        <input type='text' value={searchinput} style={{width:'80%'}} 
-                        onChange={(e) => setSearchInput(e.target.value)} 
-                        placeholder='search for movie...'/>
-                        <button className='search__btn' onClick={fetch_search}>Search</button>
-                    </div>
-                </div>
-            </div>
-            <div className='result__section'>
-                <> 
-                    <p style={{color:'white', paddingLeft:'0px'}}>{result__lenght} results found</p>  
-                    {
-                        result__lenght > 0 ? (
-                            <div className='container'>
-                                {
-                                    searchresponse.map((result) => (
-                                        <ResultCard result={result}/>
-                                    ))
-                                } 
-                            </div>
-                            ) : (
-                            <Error/>
-                        )
-                    }
-                </>
-            </div>
+  const result__lenght = searchresponse.length;
+
+  //   console.log(searchresponse);
+  return (
+    <div className="search__section">
+      <div className="search__input__section">
+        <div className="navigate_icon" onClick={() => navigate(-1)}>
+          <MdOutlineArrowBackIosNew
+            style={{ color: "white" }}
+            className="nav__icon"
+          />
         </div>
-     );
-}
- 
+        <div className="search__field">
+          <div className="input__gloss">
+            <BiSearch className="search__icon" style={{ color: "white" }} />
+            <input
+              type="text"
+              value={searchinput}
+              style={{ width: "80%" }}
+              onChange={(e) => setSearchInput(e.target.value)}
+              placeholder="search for movie..."
+            />
+            <button className="search__btn" onClick={fetch_search}>
+              Search
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="result__section">
+        <>
+          <p style={{ color: "white", paddingLeft: "0px" }}>
+            {result__lenght} results found
+          </p>
+          {result__lenght > 0 ? (
+            <div className="container">
+              {searchresponse.map((result) => (
+                <ResultCard result={result} />
+              ))}
+            </div>
+          ) : (
+            <Error />
+          )}
+        </>
+      </div>
+    </div>
+  );
+};
+
 export default Search;
