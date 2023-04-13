dd20794ebcc8d29407cfd0b3cf5fcd86a0103baa
shegz101 <bobatesegun3@gmail.com>
2023-04-14 00:59:04 +0100

    player-mode-added-to-search-page

--- src/components/ResultCard.jsx
+++ src/components/ResultCard.jsx
@@ -1,46 +1,83 @@
-import React, { useState } from 'react';
-import '../styles/search.css';
+import React, { useState } from "react";
+import "../styles/search.css";
 import errimg from "../images/400.png";
-import {FaPlayCircle} from "react-icons/fa";
-import { useNavigate } from 'react-router-dom';
+import { FaPlayCircle } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
 import movieTrailer from "movie-trailer";
+import {
+  trail,
+  movieId,
+  mname,
+  mdescp,
+  mdate,
+  mlang,
+} from "../features/authSlice.js";
 
 const ResultCard = ({ result }) => {
-    const img_url = `https://image.tmdb.org/t/p/original`;
-    const navigate = useNavigate();
-    const [trailersId, setTrailersId] = useState('');
+  const img_url = `https://image.tmdb.org/t/p/original`;
 
-    const handleResultModal = (movie) => {
-        if (trailersId) {
-            setTrailersId('');
-        } else {
-            movieTrailer(movie?.name || movie.title || movie?.original_name || "").then(url => {
-                const urlParams = new URLSearchParams(new URL(url).search);
-                setTrailersId(urlParams.get("v"));
-            }).catch((err) => console.log(err));
-        }
-        navigate('/playmore');
-    };
+  const navigate = useNavigate();
+  const [trailersId, setTrailersId] = useState("");
 
-    return (
-        <div className="movie">
-            <div>
-                <p>{result.release_date}</p>
-            </div>
+  const dispatch = useDispatch();
 
-            <div>
-                <img src={`${img_url}/${result.backdrop_path || result.poster_path}` === null ? `${errimg}`  : `${img_url}/${result.backdrop_path || result.poster_path}` } alt={result?.name}/>
-            </div>
+  const handleResultModal = (trailer) => {
+    if (trailersId) {
+      setTrailersId("");
+    } else {
+      movieTrailer(
+        trailer?.name || trailer?.title || trailer?.original_name || ""
+      )
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          dispatch(trail(urlParams.get("v")));
+          setTrailersId(urlParams.get("v"));
+          dispatch(
+            mname(
+              trailer?.name || trailer?.title || trailer?.original_name || ""
+            )
+          );
+          dispatch(movieId(trailer?.id));
+          dispatch(mdescp(trailer?.overview));
+          dispatch(mdate(trailer?.release_date));
+          dispatch(mlang(trailer?.original_language));
+        })
+        .catch((err) => console.log(err));
+    }
+    navigate("/movieshow");
+  };
 
-            <div>
-                <span onClick={() => handleResultModal(result)}><FaPlayCircle style={{width:'35px', height:'35px', color:'#e50914'}}/></span>
-            </div>
+  return (
+    <div className="movie">
+      <div>
+        <p>{result.release_date}</p>
+      </div>
 
-            <div>
-                <h3>{result.title}</h3>
-            </div>
-        </div>
-    )
-}
+      <div>
+        <img
+          src={
+            `${img_url}/${result.backdrop_path || result.poster_path}` === null
+              ? `${errimg}`
+              : `${img_url}/${result.backdrop_path || result.poster_path}`
+          }
+          alt={result?.name}
+        />
+      </div>
+
+      <div>
+        <span onClick={() => handleResultModal(result)}>
+          <FaPlayCircle
+            style={{ width: "35px", height: "35px", color: "#e50914" }}
+          />
+        </span>
+      </div>
+
+      <div>
+        <h3>{result.title}</h3>
+      </div>
+    </div>
+  );
+};
 
 export default ResultCard;
