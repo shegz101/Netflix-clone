0b9e2acfcb3f657f5b3a6ebffa9d661b6ad06aeb
shegz101 <bobatesegun3@gmail.com>
2023-04-14 01:18:45 +0100

    trailers-can-now-be-played-from-the-banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,12 +1,26 @@
 import React, { useState, useEffect } from "react";
 // import Typewriter from "typewriter-effect";
 // import { TbPlayerPlay } from "react-icons/tb";
+import movieTrailer from "movie-trailer";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import {
+  trail,
+  movieId,
+  mname,
+  mdescp,
+  mdate,
+  mlang,
+} from "../features/authSlice.js";
 import "../styles/Banner.css";
 import axios_fetch from "../api/axios_fetch";
 import request_data from "../api/request_data";
 
 const Banner = () => {
   const [bannermovie, setBannerMovie] = useState([]);
+  const [trailerId, setTrailerId] = useState("");
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const fetch_movie = async () => {
     const resp = await axios_fetch.get(request_data.Trending);
@@ -15,6 +29,28 @@ const Banner = () => {
         Math.floor(Math.random() * resp.data.results.length - 1)
       ]
     );
+  };
+
+  const playTrailer = (movie) => {
+    if (trailerId) {
+      setTrailerId("");
+    } else {
+      movieTrailer(movie?.name || movie?.title || movie?.original_name || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          dispatch(trail(urlParams.get("v")));
+          setTrailerId(urlParams.get("v"));
+          dispatch(
+            mname(movie?.name || movie?.title || movie?.original_name || "")
+          );
+          dispatch(movieId(movie?.id));
+          dispatch(mdescp(movie?.overview));
+          dispatch(mdate(movie?.release_date));
+          dispatch(mlang(movie?.original_language));
+        })
+        .catch((err) => console.log(err));
+    }
+    navigate("/movieshow");
   };
 
   //function to reduce the length of movie description
@@ -60,7 +96,12 @@ const Banner = () => {
             bannermovie?.original_name}
         </h1>
         <div className="buttons">
-          <button className="button__play">Play</button>
+          <button
+            className="button__play"
+            onClick={() => playTrailer(bannermovie)}
+          >
+            Play
+          </button>
           <button className="button__add">Add to List</button>
         </div>
         <p className="movie__description">{truncate(bannermovie?.overview)}</p>
