909d28dd6ade296381cd77c85c1e7a07139ba3bd
shegz101 <bobatesegun3@gmail.com>
2023-04-29 09:44:04 +0100

    intialized cloud firestore

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,23 @@
-import { initializeApp } from 'firebase/app';
-import {getAuth} from 'firebase/auth';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAFeNLjk3aHis6t1NIPDMsZ5ErLucWQlaA",
-    authDomain: "trailflix-e2f33.firebaseapp.com",
-    projectId: "trailflix-e2f33",
-    storageBucket: "trailflix-e2f33.appspot.com",
-    messagingSenderId: "343605304524",
-    appId: "1:343605304524:web:8f7db816226387c8bbb669",
-    measurementId: "G-V8ME7R2WET"
+  apiKey: "AIzaSyAFeNLjk3aHis6t1NIPDMsZ5ErLucWQlaA",
+  authDomain: "trailflix-e2f33.firebaseapp.com",
+  projectId: "trailflix-e2f33",
+  storageBucket: "trailflix-e2f33.appspot.com",
+  messagingSenderId: "343605304524",
+  appId: "1:343605304524:web:8f7db816226387c8bbb669",
+  measurementId: "G-V8ME7R2WET",
 };
 
 // Initialize Firebase and Firebase Authentication
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-export {auth}
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
+export { auth, db };
